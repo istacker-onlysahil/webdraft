bb43661a50d7a77197beb7f2d821690d9b31f736
istacker-onlysahil <isahilsumanjha@gmail.com>
2024-05-04 20:30:04 +0530

    Starting the project of Nidhun - Crypto , Trade Website

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,9 @@
 
 export default function Home() {
   return (<>
-    <div className="flex justify-center gap-7 items-center h-screen w-screen bg-slate-100">
-      <div className="video-wrapper">
-        <video autoPlay muted loop playsInline preload="metadata" className="md:flex hidden">
-          <source src={'https://webdraft.vercel.app' + '/Videos/coming_soon_web.mp4'} type="video/mp4" />
-        </video>
-        <video autoPlay muted loop playsInline preload="metadata" className="flex md:hidden">
-          <source src={'https://webdraft.vercel.app' + '/Videos/coming_soon_mobile.mp4'} type="video/mp4" />
-        </video>
-      </div>
+    <div className="flex justify-center gap-7 items-center h-screen w-screen bg-slate-100 overflow-hidden">
+
+    <img src="https://png.pngtree.com/png-clipart/20211024/original/pngtree-coming-soon-banner-design-with-speaker-png-image_6869715.png" className="object-contain" />
 
 
     </div>
